60a87b6a082e1fe803021014ae58cf93fb33eb1c
Simeon Naydenov <moniNaydenov@gmail.com>
2017-02-01 13:57:28 +0100

    Bug #4026 - fix icon for older browsers

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017011700;
+$plugin->version   = 2017020100;
 $plugin->requires  = 2016052300;
 $plugin->component = 'local_downloadcenter';
 $plugin->maturity  = MATURITY_STABLE;
