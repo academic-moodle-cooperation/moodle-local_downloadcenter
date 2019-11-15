07d96df3c7c924b6b7513e67f8b186deffd4b7b4
Simeon Naydenov <moniNaydenov@gmail.com>
2019-11-15 23:16:00 +0200

    Moodle 3.8 compatible version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019052000;
-$plugin->requires  = 2019052000;
+$plugin->version   = 2019111500;
+$plugin->requires  = 2019111200;
 $plugin->component = 'local_downloadcenter';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v3.7.0";
+$plugin->release   = "v3.8.0";
 
