a042728c9d70c5703deba7af77b69d2269217d1b
Simeon Naydenov <moniNaydenov@gmail.com>
2021-05-09 23:36:22 +0300

    fix codechecker issues

--- locallib.php
+++ locallib.php
@@ -502,7 +502,6 @@ class local_downloadcenter_factory {
                     require_once($CFG->dirroot . '/mod/assign/locallib.php');
                     require_once($CFG->dirroot . '/mod/assign/externallib.php');
 
-
                     if ($res->resource->allowsubmissionsfromdate < time() || $res->resource->alwaysshowdescription) {
                         $fsfiles = $fs->get_area_files($context->id, 'mod_assign', 'introattachment', 0, 'id', false);
                         foreach ($fsfiles as $file) {
