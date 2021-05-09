78d3ecffa7902f7e2a32fa52d16429c224a81e81
Simeon Naydenov <moniNaydenov@gmail.com>
2021-05-09 23:24:32 +0300

    fix error with allowsubmissionsfromdate

--- locallib.php
+++ locallib.php
@@ -502,7 +502,8 @@ class local_downloadcenter_factory {
                     require_once($CFG->dirroot . '/mod/assign/locallib.php');
                     require_once($CFG->dirroot . '/mod/assign/externallib.php');
 
-                    if ($res->resource->alwaysshowdescription) {
+
+                    if ($res->resource->allowsubmissionsfromdate < time() || $res->resource->alwaysshowdescription) {
                         $fsfiles = $fs->get_area_files($context->id, 'mod_assign', 'introattachment', 0, 'id', false);
                         foreach ($fsfiles as $file) {
                             if ($file->get_filesize() == 0) {
