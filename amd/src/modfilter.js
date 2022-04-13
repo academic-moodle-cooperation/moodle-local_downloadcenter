36eac36c02bf552493afeb65f2a4f64752ebff5e
Simeon Naydenov <moniNaydenov@gmail.com>
2022-04-13 14:35:58 +0300

    change location and fix javascript

--- amd/src/modfilter.js
+++ amd/src/modfilter.js
@@ -2,7 +2,6 @@
  * Handles filtering of items
  *
  * @module        local_downloadcenter/modfilter
- * @package
  * @author        Simeon Naydenov (moniNaydenov@gmail.com)
  * @copyright     2022 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license       http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
