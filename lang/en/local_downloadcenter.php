c3ffe8a6d37d5449d83136f418edf6be7288334f
Simeon Naydenov <moniNaydenov@gmail.com>
2022-04-20 11:37:51 +0300

    #7153 - update hinweis text

--- lang/en/local_downloadcenter.php
+++ lang/en/local_downloadcenter.php
@@ -36,7 +36,7 @@ $string['settings_title'] = 'Download center';
 $string['exclude_empty_topics'] = 'Exclude empty topics';
 $string['exclude_empty_topics_help'] = 'Exclude empty topics from the downloaded zip.';
 
-$string['warningmessage'] = '<b>NOTE:</b> Download center only enables you to download visible files and folders!';
+$string['warningmessage'] = 'Here you can download single or all available contents of this course in a ZIP archive.';
 
 $string['createzip'] = 'Create ZIP archive';
 
