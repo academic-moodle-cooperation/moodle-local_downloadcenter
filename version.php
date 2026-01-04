ff142748d5fb6f44ac72920f6489a724571ac9ac
Clemens Marx <clemens.marx@gmail.com>
2026-01-04 15:29:29 +0200

    #8650 Increase Version Number for version 5.0

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025052200;
+$plugin->version   = 2025052201;
 $plugin->requires  = 2025041400;
 $plugin->component = 'local_downloadcenter';
 $plugin->maturity  = MATURITY_STABLE;
